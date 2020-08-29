483cc4f274c025aa02e9855fc5a5de70a16715f4
StivenVanegas <nevits28@hotmail.com>
2020-08-28 20:05:24 -0500

    avance Registry

--- ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -4,6 +4,13 @@ public class Registry {
     public RegisterResult registerVoter(Person p) {
 
         // TODO Validate person and return real result.
+		if(!p.isAlive()){
+			return RegisterResult.DEAD;
+		} else if(p.getAge() < 18){
+			return RegisterResult.UNDERAGE;
+		} else if(p.getAge()<0 || p.getAge() > 122){
+			return RegisterResult.INVALID_AGE;
+		}
         return RegisterResult.VALID;
     }
 }
